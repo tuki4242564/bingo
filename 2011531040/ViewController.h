7d5a5d7c04b21b726f24ecd93470f6fd2d1a8097
tuki4242564 <tuki4242564@yahoo.co.jp>
2014-01-23 13:21:20 +0900

    2014・1・23

--- 2011531040/ViewController.h
+++ 2011531040/ViewController.h
@@ -8,6 +8,28 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
 
+    IBOutlet UIButton *Lover;
+    IBOutlet UIButton *Mover;
+    IBOutlet UIButton *Rover;
+    IBOutlet UIButton *L;
+    IBOutlet UIButton *M;
+    IBOutlet UIButton *R;
+    IBOutlet UIButton *Lunder;
+    IBOutlet UIButton *Munder;
+    IBOutlet UIButton *Runder;
+    int a,b,c,d,e,f,g,h,i;
+    IBOutlet UILabel *question;
+    IBOutlet UIButton *Y;
+    IBOutlet UIButton *N;
+    IBOutlet UILabel *right1;
+    IBOutlet UILabel *right2;
+    IBOutlet UIButton *ResetButton;
+    IBOutlet UIButton *next;
+}
+    -(IBAction)ninemove:(id)sender;
+    -(IBAction)answer:(id)sender;
+    -(IBAction)reset:(id)sender;
+-(IBAction)nextscrean:(id)sender;
 @end
--- 2011531040/ViewController.m
+++ 2011531040/ViewController.m
@@ -16,7 +16,15 @@
 
 - (void)viewDidLoad
 {
+    [question setFont:[UIFont boldSystemFontOfSize:30.0f]];
+    [question setTextColor:[UIColor blackColor]];
     [super viewDidLoad];
+    Y.hidden=YES;
+    N.hidden=YES;
+    question.hidden=YES;
+    right1.hidden=YES;
+    right2.hidden=YES;
+    next.hidden=YES;
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,6 +32,140 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+-(IBAction)ninemove:(id)sender{
+    UIButton *z = (UIButton *)sender;
+    if(z.tag==1){
+        question.text=@"Q.1 : ~~~~~~~~~~~~?";
+        L.hidden=YES;
+        M.hidden=YES;
+        R.hidden=YES;
+        Lover.hidden=YES;
+        Mover.hidden=YES;
+        Rover.hidden=YES;
+        Lunder.hidden=YES;
+        Munder.hidden=YES;
+        Runder.hidden=YES;
+        Y.hidden=NO;
+        N.hidden=NO;
+        question.hidden=NO;
+    }
+if(z.tag==2){
+   // right1.hidden=YES;
+    question.text=@"Q.2 : ~~~~~~~~~~~~~?";
+    L.hidden=YES;
+    M.hidden=YES;
+    R.hidden=YES;
+    Lover.hidden=YES;
+    Mover.hidden=YES;
+    Rover.hidden=YES;
+    Lunder.hidden=YES;
+    Munder.hidden=YES;
+    Runder.hidden=YES;
+    Y.hidden=NO;
+    N.hidden=NO;
+    question.hidden=NO;
+  }
+    if(z.tag==3){
+       // right1.hidden=YES;
+        question.text=@"Q.3 : ~~~~~~~~~~~~?";
+        L.hidden=YES;
+        M.hidden=YES;
+        R.hidden=YES;
+        Lover.hidden=YES;
+        Mover.hidden=YES;
+        Rover.hidden=YES;
+        Lunder.hidden=YES;
+        Munder.hidden=YES;
+        Runder.hidden=YES;
+        Y.hidden=NO;
+        N.hidden=NO;
+        question.hidden=NO;
+    }
+}
+
+
+
+
+
+
+-(IBAction)answer:(id)sender{
+    UIButton *z = (UIButton *)sender;
+    if(z.tag==11 & a==0 &b==0 && c==0){
+        Y.hidden=YES;
+        N.hidden=YES;
+        next.hidden=NO;
+        question.text=@"正解";
+        a=1;
+    }else if(z.tag==10 && a==0 &b==0 && c==0){
+     question.text=@"は ず　れ";
+        Y.hidden=YES;
+        N.hidden=YES;
+        next.hidden=YES;
+    } 
+    
+    if(z.tag==11 && a==1 &b==0 && c==0){
+       // right1.hidden=NO;
+        //right2.hidden=NO;
+        Y.hidden=YES;
+        N.hidden=YES;
+        next.hidden=NO;
+        question.text=@"正解";
+        b=1;
+    }else if(z.tag==10 && a==1 &b==0 && c==0){
+        question.text=@"は　ず　れ";
+        Y.hidden=YES;
+        N.hidden=YES;
+        next.hidden=YES;
+    }  
+    if(z.tag==11 && a==1 &b==1 && c==0){
+        //right1.hidden=YES;
+        //right2.hidden=YES;
+        Y.hidden=YES;
+        N.hidden=YES;
+        next.hidden=NO;
+        question.text=@"クリア";
+    }else if(z.tag==10 && a==1 &b==1 && c==1){
+        question.text=@"は　ず れ";
+        Y.hidden=YES;
+        N.hidden=YES;
+        next.hidden=YES;
+    }}
+    
+
+ -(IBAction)nextscrean:(id)sender{
+     L.hidden=NO;
+     M.hidden=NO;
+     R.hidden=NO;
+     Lover.hidden=NO;
+     Mover.hidden=NO;
+     Rover.hidden=NO;
+     Lunder.hidden=NO;
+     Munder.hidden=NO;
+     Runder.hidden=NO;
+     c=1;
+ }
+
+
+   -(IBAction)reset:(id)sender{
+       next.hidden=YES;
+       Y.hidden=YES;
+       N.hidden=YES;
+       question.hidden=YES;
+       right1.hidden=YES;
+       right2.hidden=YES;
+       L.hidden=NO;
+       M.hidden=NO;
+       R.hidden=NO;
+       Lover.hidden=NO;
+       Mover.hidden=NO;
+       Rover.hidden=NO;
+       Lunder.hidden=NO;
+       Munder.hidden=NO;
+       Runder.hidden=NO;
+       a=b=c=d=e=f=g=h=i=0;
+       
+    
 }
 
 @end
